20285bf547ecf099f9d2989ed9b6b495274c88fa
Kévin Jin <kevin1501@hotmail.fr>
2024-05-27 15:15:21 +0200

    Ajout d'une 2eme API permettant de récuperer la liste des 15 plus grandes villes de france.

--- Program.cs
+++ Program.cs
@@ -1,17 +1,54 @@
-﻿using System.Net.Http.Json;
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
 using System.Text.Json.Serialization;
+using System.Threading;
+using System.Threading.Tasks;
 
 internal class Program
 {
     private static async Task Main(string[] args)
     {
-        ThreadPool.QueueUserWorkItem(GetUrl);
+        await GetCitiesWithHighestPopulationInFranceAsync(15);
+        ThreadPool.QueueUserWorkItem(async state => await GetAirQualityAsync("paris"));
         Console.ReadLine();
     }
 
-    private async static void GetUrl(object state)
+    private async static Task GetCitiesWithHighestPopulationInFranceAsync(int limit)
     {
-        string city = "paris";
+        using HttpClient client = new HttpClient();
+        client.DefaultRequestHeaders.Add("X-Api-Key", "aD5jOB3U948A4Mm6b/VuXw==1egSxgkPiCe4yUZ3");  // Remplacez par votre clé API de Ninja
+
+        var response = await client.GetAsync($"https://api.api-ninjas.com/v1/city?country=FR&limit={limit}");
+
+        if (response.IsSuccessStatusCode)
+        {
+            var cities = await response.Content.ReadFromJsonAsync<CityResponse[]>();
+            if (cities != null && cities.Length > 0)
+            {
+                foreach (var city in cities)
+                {
+                    Console.WriteLine($"City: {city.Name}");
+                    Console.WriteLine($"Population: {city.Population}");
+                    Console.WriteLine();
+                }
+            }
+            else
+            {
+                Console.WriteLine("No cities found in France.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Error fetching data");
+            var errorContent = await response.Content.ReadAsStringAsync();
+            Console.WriteLine($"Error details: {errorContent}");
+        }
+    }
+
+    private async static Task GetAirQualityAsync(string city)
+    {
         HttpClient client = new HttpClient();
         client.BaseAddress = new Uri("https://api.waqi.info/");
         var response = await client.GetAsync($"feed/" + city + "/?token=9f08b89a29ddf8fad59694a44fc0c5edf25a89dd");
@@ -26,6 +63,15 @@ internal class Program
         {
             Console.WriteLine("Error fetching data");
         }
+    }
+
+    public class CityResponse
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("population")]
+        public int Population { get; set; }
     }
 
     public class AirQualityResponse
